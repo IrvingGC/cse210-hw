ef73a9ecdb6cb7664a89a992e4ca66a4e92a749d
IrvingGC <irving.50505@gmail.com>
2025-02-19 18:51:22 -0600

    finished

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,123 @@
 using System;
+using System.Collections.Generic;
+
+public abstract class Activity
+{
+    public DateTime Date { get; set; }
+    public int LengthInMinutes { get; set; }
+
+    public Activity(DateTime date, int lengthInMinutes)
+    {
+        Date = date;
+        LengthInMinutes = lengthInMinutes;
+    }
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public string GetSummary()
+    {
+        return $"{Date:dd MMM yyyy} {GetType().Name} ({LengthInMinutes} min) - Distance {GetDistance():0.0} miles, Speed {GetSpeed():0.0} mph, Pace: {GetPace():0.0} min per mile";
+    }
+}
+
+public class Running : Activity
+{
+    public double DistanceInMiles { get; set; }
+
+
+    public Running(DateTime date, int lengthInMinutes, double distanceInMiles)
+        : base(date, lengthInMinutes)
+    {
+        DistanceInMiles = distanceInMiles;
+    }
+
+    public override double GetDistance()
+    {
+        return DistanceInMiles;
+    }
+
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / LengthInMinutes) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return LengthInMinutes / GetDistance();
+    }
+}
+
+public class Cycling : Activity
+{
+    public double SpeedInMph { get; set; }
+
+    public Cycling(DateTime date, int lengthInMinutes, double speedInMph)
+        : base(date, lengthInMinutes)
+    {
+        SpeedInMph = speedInMph;
+    }
+
+    public override double GetDistance()
+    {
+        return (SpeedInMph * LengthInMinutes) / 60;
+    }
+
+    public override double GetSpeed()
+    {
+        return SpeedInMph;
+    }
+
+    public override double GetPace()
+    {
+        return 60 / GetSpeed();
+    }
+}
+
+public class Swimming : Activity
+{
+    public int Laps { get; set; }
+
+    // Constructor
+    public Swimming(DateTime date, int lengthInMinutes, int laps)
+        : base(date, lengthInMinutes)
+    {
+        Laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return (Laps * 50 * 0.62) / 1000;
+    }
+
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / LengthInMinutes) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return LengthInMinutes / GetDistance();
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2024, 12, 3), 60, 5.0),
+            new Cycling(new DateTime(2024, 12, 3), 30, 12.0),
+            new Swimming(new DateTime(2024, 12, 3), 60, 20)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
-}
+}    
